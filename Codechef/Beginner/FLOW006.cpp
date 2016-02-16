7b307f3a5f5452b0e771f82525590bf0eb1134cf
Ganesh S Kudva <ganeshskudva@gmail.com>
2016-02-15 18:45:43 -0800

    Practice problem
    Codechef Practice problem

---
+++ Codechef/Beginner/FLOW006.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+typedef long long int ll;
+
+using namespace std;
+
+#define GETCHAR getchar_unlocked
+inline ll readInt() 
+{
+    ll n = 0;
+    char c;
+    while (1)
+    { 
+		c=GETCHAR();
+		if(c=='\n'||c==' ') break;
+		n = n * 10 + c - '0';
+    }
+    return n;
+}
+
+ll get_sum(ll n)
+{
+	ll sum = 0;
+
+	while (n)
+	{
+		sum += n%10;
+		n /= 10;
+	}
+
+	return sum;
+}
+
+int main()
+{
+	ll t,n;
+
+	t = readInt();
+
+	while (t--)
+	{
+		n = readInt();
+		printf("%lld\n",get_sum(n));
+	}
+
+	return 0;
+}
