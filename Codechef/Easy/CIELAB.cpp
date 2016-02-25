4773de4e22e6287d323828ea623d25cb717e9424
Ganesh S Kudva <ganeshskudva@gmail.com>
2016-02-24 21:22:48 -0800

    Practice problem
    Code chef practice

---
+++ Codechef/Easy/CIELAB.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int A,B,result;
+    scanf("%d %d",&A , &B);
+    result = ( (A - B) % 10 != 9) ? (A - B + 1):(A - B - 1);
+    printf("%d\n",result);
+
+    return 0;
+}
