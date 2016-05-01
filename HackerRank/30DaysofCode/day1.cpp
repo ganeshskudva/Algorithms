3c94300f667ff6d22ec8cb6e500f80afe1f09679
Ganesh S Kudva <ganeshskudva@gmail.com>
2016-05-01 16:29:24 -0700

    Day 1
    30 Days of Code

---
+++ HackerRank/30DaysofCode/day1.cpp
@@ -0,0 +1,59 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <set>
+#include <map>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    // Declare second integer, double, and String variables.
+    int myInt = 0;
+    double myDouble = 0.0;
+    string str;
+    // Read and save an integer, double, and String to your variables.
+    cin >> myInt>>myDouble;
+
+    cin.ignore();
+    getline(cin, str);
+    // Print the sum of both integer variables on a new line.
+    cout<<i + myInt<<endl;
+    // Print the sum of the double variables on a new line.
+    printf("%0.1lf\n",d + myDouble);
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    cout<<s + str<<endl;
+
+    return 0;
+}
