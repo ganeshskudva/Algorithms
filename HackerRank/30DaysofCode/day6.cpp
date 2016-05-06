6dab8ed4be7244660f3474e5e82a575cbca19ffe
Ganesh S Kudva <ganeshskudva@gmail.com>
2016-05-06 10:35:37 -0700

    Day 6
    Day 6

---
+++ HackerRank/30DaysofCode/day6.cpp
@@ -0,0 +1,66 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <set>
+#include <map>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+
+int main()
+{
+	ull t,i = 0;
+	string str;
+#if DEBUG 
+	srand((unsigned)time(0));
+	t = (rand()%10)+1; 
+	cout <<t<<endl;
+#else 
+	cin >> t;
+#endif 
+
+	while (t--)
+	{
+		cin >> str;
+		
+		for (i = 0; i < str.size(); i += 2)
+		{
+			cout<<str[i];
+		}
+		cout<<" ";
+
+		for (i = 1; i < str.size(); i += 2)
+		{
+			cout<<str[i];
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}
