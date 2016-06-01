037c0dd53beb8f024f22ca1ec3c76e8599ea9e61
Ganesh S Kudva <ganeshskudva@gmail.com>
2016-06-01 14:36:08 -0700

    June 2016 Easy
    June 2016 Easy

---
+++ HackerEarth/Benny_School_Report.cpp
@@ -0,0 +1,77 @@
+/*
+* AUTHOR: Ganesh S Kudva
+*/
+
+#include <bits/stdc++.h>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+
+int main()
+{
+	ull t = 0,n,m,x,i,sum, needed;
+#if DEBUG 
+	srand((unsigned)time(0));
+	t = (rand()%10)+1; 
+	cout <<t<<endl;
+#else 
+	cin >> t;
+#endif 
+
+	while (t--)
+	{
+		sum = 0;
+		needed = 0;
+		cin >> n>>m>>x;
+		ull arr[n];
+		MEM(arr, 0, n, ull);
+
+		FORN(i, n)
+		{
+			cin >> arr[i];
+			sum += arr[i];
+		}
+
+		if ((sum / (n + 1)) < x )
+		{
+			needed = (x * (n + 1)) - sum;
+
+			if (needed > m)
+			{
+				cout << "Impossible"<<endl;
+			}
+			else
+			{
+				cout << needed << endl;
+			}
+		}
+		else
+		{
+			cout << 1 << endl;
+		}
+	}
+
+	return 0;
+}
