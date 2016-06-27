540df5358f7f6399bf517b2efdbc72a79aad9ef6
Ganesh S Kudva <ganeshskudva@gmail.com>
2016-06-26 21:19:06 -0700

    Practice Problem
    Practice Problem

---
+++ HackerEarth/Complete_String.cpp
@@ -0,0 +1,70 @@
+/*
+* AUTHOR: Ganesh S Kudva
+*/
+
+#include <bits/stdc++.h>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+
+int main()
+{
+	ull t = 0,i,j;
+	set<char>  input;
+	bool possible = true;
+	string str;
+#if DEBUG 
+	srand((unsigned)time(0));
+	t = (rand()%10)+1; 
+	cout <<t<<endl;
+#else 
+	cin >> t;
+#endif 
+
+
+	while (t--)
+	{
+		cin >> str;
+		possible = true;
+		FORN(i, str.size())
+		{
+			input.insert(str[i]);
+		}
+
+		for (i = 97; i <= 122; i++)
+		{
+			if (input.count(i) == 0)
+			{
+				possible = false;
+				break;
+			}
+		}
+
+		printf("%s\n", possible?"YES":"NO");
+		input.clear();
+	}
+
+	return 0;
+}
