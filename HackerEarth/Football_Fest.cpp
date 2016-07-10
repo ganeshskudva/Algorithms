fa23b13eef7c2b6ab5f6ba9e886a40465df537e9
Ganesh S Kudva <ganeshskudva@gmail.com>
2016-07-10 16:50:05 -0700

    Practice Problem
    Practice Problem

---
+++ HackerEarth/Football_Fest.cpp
@@ -0,0 +1,75 @@
+/*
+* AUTHOR: Ganesh S Kudva
+*/
+
+#include <bits/stdc++.h>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+
+int main()
+{
+	ull t = 0,i,j, n, id, prev;
+	stack<ull> pass;
+	char c;
+#if DEBUG 
+	srand((unsigned)time(0));
+	t = (rand()%10)+1; 
+	cout <<t<<endl;
+#else 
+	cin >> t;
+#endif 
+
+	while (t--)
+	{
+		cin >> n >> id;
+		
+		ull arr[n+1];
+		MEM(arr, 0, (n+1), ull);
+		arr[0] = id;
+		FORN(i, n)
+		{
+			cin >> c;
+
+
+			if (c == 'P')
+			{
+				cin >> id;
+				arr[i+1] = id;
+			}
+			else
+			{
+				prev = arr[i - 1];
+				arr[i+1] = arr[i - 1];
+			}
+		}
+		cout << "Player "<<arr[n]<<endl;
+		
+	}
+
+
+
+	return 0;
+}
