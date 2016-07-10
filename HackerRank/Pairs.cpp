f618b6ea1a534008b8ac7eccb67cb83d96687301
Ganesh S Kudva <ganeshskudva@gmail.com>
2016-07-10 12:07:54 -0700

    Practice Problem
    Practice Problem

---
+++ HackerRank/Pairs.cpp
@@ -0,0 +1,72 @@
+/*
+* AUTHOR: Ganesh S Kudva
+* https://www.hackerrank.com/challenges/pairs
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <set>
+#include <map>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+
+int main()
+{
+	ull t = 0,i,j, n, val, k, cnt;
+	vector<ull> vec;
+	set<ull> num;
+#if DEBUG 
+	srand((unsigned)time(0));
+	t = (rand()%10)+1; 
+	cout <<t<<endl;
+#else 
+	cin >> n >> k;
+#endif 
+
+	FORN(i, n)
+	{
+		cin >> val;
+		vec.push_back(val);
+		num.insert(val);
+	}
+
+	cnt = 0;
+	FORN(i, n)
+	{
+		if (num.count(k + vec[i]) != 0)
+		{
+			cnt++;
+		}
+	}
+	cout << cnt << endl;
+
+	return 0;
+}
