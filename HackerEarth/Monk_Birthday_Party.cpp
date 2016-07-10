58130d7ad6e78f7609b611c3a625532f754d4f1b
Ganesh S Kudva <ganeshskudva@gmail.com>
2016-07-10 11:55:51 -0700

    Practice Problem
    Practice Problem

---
+++ HackerEarth/Monk_Birthday_Party.cpp
@@ -0,0 +1,67 @@
+/*
+* AUTHOR: Ganesh S Kudva
+* https://www.hackerearth.com/problem/algorithm/monks-birthday-party/
+*/
+
+#include <bits/stdc++.h>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+
+int main()
+{
+	ull t = 0,i,j, n;
+	set<string> names;
+	string str;
+#if DEBUG 
+	srand((unsigned)time(0));
+	t = (rand()%10)+1; 
+	cout <<t<<endl;
+#else 
+	cin >> t;
+#endif 
+
+	while (t--)
+	{
+		cin >> n;
+
+		FORN(i, n)
+		{
+			cin >> str;
+			if (names.count(str) == 0)
+			{
+				names.insert(str);
+			}
+		}
+
+		for (set<string>::iterator it = names.begin(); it != names.end(); ++it)
+		{
+			cout << *it << endl;
+		}
+		names.clear();
+	}
+
+	return 0;
+}
