e15f761e4c2382b54cab7e04e3ec6ac7060218ba
Ganesh S Kudva <ganeshskudva@gmail.com>
2016-07-14 06:36:52 -0700

    Practice Problem
    Practice Problem

---
+++ HackerEarth/Biased_Chandan.cpp
@@ -0,0 +1,72 @@
+/*
+* AUTHOR: Ganesh S Kudva
+*/
+
+#include <bits/stdc++.h>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+
+int main()
+{
+	ull t = 0,i,j, n, sum, val;
+	stack<ull> cand;
+#if DEBUG 
+	srand((unsigned)time(0));
+	t = (rand()%10)+1; 
+	cout <<t<<endl;
+#else 
+	cin >> n;
+#endif 
+
+
+	sum = 0;
+	FORN(i, n)
+	{
+		cin >> val;
+
+		if (!val)
+		{
+			if (!cand.empty())
+			{
+				cand.pop();
+			}
+		}
+		else
+		{
+			cand.push(val);
+		}
+	}
+
+
+	while (!cand.empty())
+	{
+		sum += cand.top();
+		cand.pop();
+	}
+	cout << sum << endl;
+
+	return 0;
+}
