57883b04456904c37d5f1d8e1cbdaf4d9cb6f1df
Ganesh S Kudva <ganeshskudva@gmail.com>
2016-07-16 14:47:15 -0700

    Practice Problem
    Practice Problem

---
+++ HackerRank/Angry_Professor.cpp
@@ -0,0 +1,78 @@
+/*
+* AUTHOR: Ganesh S Kudva
+* https://www.hackerrank.com/challenges/angry-professor
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <set>
+#include <map>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+
+int main()
+{
+	ull t = 0,i,j, n, k, cnt, val;
+#if DEBUG 
+	srand((unsigned)time(0));
+	t = (rand()%10)+1; 
+	cout <<t<<endl;
+#else 
+	cin >> t;
+#endif 
+
+	while (t--)
+	{
+		cnt = 0;
+		cin >> n >> k;
+
+		FORN(i, n)
+		{
+			cin >> val;
+
+			if (val <= 0)
+			{
+				cnt++;
+			}
+		}
+
+		if (cnt >= k)
+		{
+			cout << "NO"<<endl;
+		}
+		else
+		{
+			cout << "YES" << endl;
+		}
+	}
+
+	return 0;
+}
