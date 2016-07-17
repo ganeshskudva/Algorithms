9ef5445aa1938c2aa29385bf80c38198f2b757dd
Ganesh S Kudva <ganeshskudva@gmail.com>
2016-07-17 11:09:56 -0700

    Practice Problem
    Practice Problem

---
+++ HackerEarth/Unique_Strings.cpp
@@ -0,0 +1,64 @@
+/*
+* AUTHOR: Ganesh S Kudva
+*/
+
+#include <bits/stdc++.h>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+
+int main()
+{
+	ull t = 0,i,j, size;
+	string str, out;
+	set<char> charSet;
+#if DEBUG 
+	srand((unsigned)time(0));
+	t = (rand()%10)+1; 
+	cout <<t<<endl;
+#else 
+	cin >> t;
+#endif 
+
+	while (t--)
+	{
+		cin >> str;
+		size = str.size();
+
+		FORN(i, size)
+		{
+			if (charSet.count(str[i]) == 0)
+			{
+				charSet.insert(str[i]);
+				out.push_back(str[i]);
+			}
+		}
+		cout << out << endl;
+		out.clear();
+		charSet.clear();
+	}
+
+	return 0;
+}
