b1ebd5391b627e62296f96a563dc21d174e7a459
Ganesh S Kudva <ganeshskudva@gmail.com>
2016-08-06 22:22:07 -0700

    Practice Problem
    Practice Problem

---
+++ Codechef/Easy/CHEFSTON.cpp
@@ -0,0 +1,85 @@
+/*
+* AUTHOR: Ganesh S Kudva
+*/
+
+#include <bits/stdc++.h>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+#define CLOCK 0
+
+int main()
+{
+#if CLOCK
+	clock_t tStart = clock();
+#endif
+	ull t = 0,i,j, n, k, val, max, num, profit;
+	vector<ull> timevec, cost;
+#if DEBUG 
+	srand((unsigned)time(0));
+	t = (rand()%10)+1; 
+	cout <<t<<endl;
+#else 
+	cin >> t;
+#endif 
+
+	while (t--)
+	{
+		cin >> n >> k;
+		timevec.clear();
+		cost.clear();
+		FORN(i, n)
+		{
+			cin >> val;
+			timevec.push_back(val);
+		}
+
+		FORN(i, n)
+		{
+			cin >> val;
+			cost.push_back(val);
+		}
+
+		max = -1;
+
+		FORN(i, n)
+		{
+			num = k/timevec[i];
+			profit = num * cost[i];
+
+			if (profit > max)
+			{
+				max = profit;
+			}
+		}
+		cout << max << endl;
+	}
+
+
+#if CLOCK
+	printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+#endif
+	return 0;
+}
