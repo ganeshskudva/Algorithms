9f7c3a8de992ce765ce1925cd7ad6eb199ae8e24
Ganesh S Kudva <ganeshskudva@gmail.com>
2017-01-25 23:29:53 -0800

    Practice Problem
    Practice Problem

---
+++ HackerRank/Plus_Minuc.cpp
@@ -0,0 +1,85 @@
+/*
+* AUTHOR: Ganesh S Kudva
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <set>
+#include <map>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+#define CLOCK 0
+
+int main()
+{
+#if CLOCK
+	clock_t tStart = clock();
+#endif
+	ull t = 0,i,j, n, val, pos, neg, zero;
+	vector<ull> vec;
+	string str;
+#if DEBUG 
+	srand((unsigned)time(0));
+	t = (rand()%10)+1; 
+	cout <<t<<endl;
+#else 
+	cin >> t;
+#endif 
+
+	pos = 0;
+	neg = 0;
+	zero = 0;
+
+	FORN(i, t)
+	{
+		cin >> val;
+
+		if (val > 0)
+		{
+			pos++;
+		}
+		if (val < 0)
+		{
+			neg++;
+		}
+		if (val == 0)
+		{
+			zero++;
+		}
+	}
+
+	cout << (double)pos/t << endl << (double)neg/t << endl << (double)zero/t<<endl;
+
+#if CLOCK
+	printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+#endif
+	return 0;
+}
