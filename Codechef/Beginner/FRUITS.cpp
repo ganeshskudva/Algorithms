79e962b007910b2c02dc1e16cfaa5caee6599b68
Ganesh S Kudva <ganeshskudva@gmail.com>
2017-02-09 14:22:36 -0800

    Practice Problem
    Practice Problem

---
+++ Codechef/Beginner/FRUITS.cpp
@@ -0,0 +1,84 @@
+/*
+* AUTHOR: Ganesh S Kudva
+*/
+
+#include <bits/stdc++.h>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+#define CLOCK 0
+
+ull min (ull a, ull b)
+{
+	if (a < b)
+	{
+		return b - a;
+	}
+
+	return a - b;
+}
+
+int main()
+{
+#if CLOCK
+	clock_t tStart = clock();
+#endif
+	ull t = 0,i,j, n,m,k, val, cnt;
+#if DEBUG 
+	srand((unsigned)time(0));
+	t = (rand()%10)+1; 
+	cout <<t<<endl;
+#else 
+	cin >> t;
+#endif 
+
+	while (t--)
+	{
+#if DEBUG
+		n = (rand()%100)+1; 
+		m = (rand()%100)+1;
+		k = (rand()%10)+1;
+		cout << n <<" "<<m<<" "<<k<<endl;
+#else
+		cin >> n >> m >> k;
+#endif
+
+		val = min (n, m);
+		if (val-k < 0)
+		{
+			cout << "0" << endl;
+		}
+		else
+		{
+			cout << val- k << endl;
+		}
+	}
+
+
+#if CLOCK
+	printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+#endif
+	return 0;
+}
