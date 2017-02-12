b75f1696bfaec4e3b9af6e30a1d4d5f8cc5c5340
Ganesh S Kudva <ganeshskudva@gmail.com>
2017-02-11 17:11:04 -0800

    Practice Problem
    Practice Problem

---
+++ Codechef/Beginner/AMR15a.cpp
@@ -0,0 +1,70 @@
+/*
+* AUTHOR: Ganesh S Kudva
+*/
+
+#include <bits/stdc++.h>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+#define CLOCK 0
+
+int main()
+{
+#if CLOCK
+	clock_t tStart = clock();
+#endif
+	ull t = 0,i,j, n, val, cntE, cntO;
+#if DEBUG 
+	srand((unsigned)time(0));
+	t = (rand()%10)+1; 
+	cout <<t<<endl;
+#else 
+	cin >> t;
+#endif 
+
+	cntE = 0;
+	cntO = 0;
+	FORN(i, t)
+	{
+		cin >> val;
+
+		if (val & 1)
+		{
+			cntO++;
+		}
+		else
+		{
+			cntE++;
+		}
+	}
+
+	printf("%s\n", cntE>cntO?"READY FOR BATTLE":"NOT READY");
+
+
+#if CLOCK
+	printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+#endif
+	return 0;
+}
