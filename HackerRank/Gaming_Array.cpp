c3112aafcac9464a99a308cb3935be5523d3a0be
Ganesh S Kudva <ganeshskudva@gmail.com>
2017-06-13 21:21:20 -0700

    Practice Problem
    Practice Problem

---
+++ HackerRank/Gaming_Array.cpp
@@ -0,0 +1,93 @@
+/*
+* AUTHOR: Ganesh S Kudva
+*/
+
+#include <bits/stdc++.h>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+#define CLOCK 0
+#define DUMMY_MAX -100000000
+
+bool isPrime(ull number){
+
+    if(number < 2) return false;
+    if(number == 2) return true;
+    if(number % 2 == 0) return false;
+    for(int i=3; (i*i)<=number; i+=2){
+        if(number % i == 0 ) return false;
+    }
+    return true;
+
+}
+
+static inline ull is_odd(ull x) { return x & 1; }
+
+int main()
+{
+#if CLOCK
+	clock_t tStart = clock();
+#endif
+	ull t = 0,i,j, n, val, cnt, max;
+#if DEBUG 
+	srand((unsigned)time(0));
+	t = (rand()%10)+1; 
+	cout <<t<<endl;
+#else 
+	cin >> t;
+#endif 
+
+	while (t--)
+	{
+		cin >> n;
+		cnt = 0;
+		max = DUMMY_MAX;
+		FORN(i, n)
+		{
+			cin >> val;
+			if (val > max)
+			{
+				max = val;
+				cnt++;
+			}
+		}
+
+		if (is_odd(cnt))
+		{
+			cout << "BOB"<<endl;
+		}
+		else
+		{
+			cout << "ANDY"<<endl;
+
+		}
+	}
+
+
+#if CLOCK
+	printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+#endif
+	return 0;
+}
