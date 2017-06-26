521f4fc6b9e857bbd030392cfdda33f5477cf6b7
Ganesh S Kudva <ganeshskudva@gmail.com>
2017-06-26 06:33:47 -0700

    Practice Problem
    Practice Problem

---
+++ HackerRank/Birthday_Chocolate.cpp
@@ -0,0 +1,96 @@
+/*
+* AUTHOR: Ganesh S Kudva
+* https://www.hackerrank.com/challenges/the-birthday-bar
+*/
+
+#include <bits/stdc++.h>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
+
+using namespace std;
+
+#define DEBUG 0
+#define CLOCK 0
+
+bool isPrime(ull number){
+
+    if(number < 2) return false;
+    if(number == 2) return true;
+    if(number % 2 == 0) return false;
+    for(int i=3; (i*i)<=number; i+=2){
+        if(number % i == 0 ) return false;
+    }
+    return true;
+
+}
+
+static inline ull is_odd(ull x) { return x & 1; }
+
+int main()
+{
+#if CLOCK
+	clock_t tStart = clock();
+#endif
+	ull t = 0,i,j, n, val, cnt, m, d, sum;
+	string str;
+	vector<ull> vec;
+#if DEBUG 
+	srand((unsigned)time(0));
+	t = (rand()%10)+1; 
+	cout <<t<<endl;
+#else 
+	cin >> n;
+#endif 
+
+	FORN(i, n)
+	{
+		cin >> val;
+		vec.push_back(val);
+	}
+	cin >> d >> m;
+	cnt = 0;
+	FORN(i, n)
+	{
+		val = m-1;
+		j = i+1;
+		sum = vec[i];
+		while ((j < n) && val)
+		{
+			sum += vec[j];
+			val--;
+			j++;
+		}
+
+		if (sum == d)
+		{
+			cnt++;
+		}
+	}
+
+	cout << cnt << endl;
+
+
+#if CLOCK
+	printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+#endif
+	return 0;
+}
