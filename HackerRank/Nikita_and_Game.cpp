27b0a6d38ba76fcdc7dbf3eefc6c8c3386f329be
Ganesh S Kudva <ganeshskudva@gmail.com>
2017-07-04 15:02:33 -0700

    Practice problem
    Practice problem

---
+++ HackerRank/Nikita_and_Game.cpp
@@ -0,0 +1,61 @@
+/*
+* AUTHOR: Ganesh S Kudva
+* https://www.hackerrank.com/challenges/array-splitting
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+long long a[100010];
+
+ll sum(int l, int r) {
+    return a[r] - a[l-1];
+}
+
+int get(int l, int r, ll s) {
+    int low, high, mid;
+    low = l;
+    high = r;
+    while( low <= high ) {
+        mid = ( low + high ) / 2;
+        ll x = sum(l, mid);
+        if( x == s && (mid == l || sum(l, mid-1) != s )) {
+            return mid;
+        } else if( x >= s ) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int solve(int l,int r){
+    ll s = sum(l, r);
+    if( l != r && s % 2 == 0 ) {
+        int ind = get(l, r, s/2);
+        if( ind != -1 ) {
+            return max(solve(l, ind), solve(ind+1, r)) + 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    int t;
+    cin>>t;
+    while (t--){
+        int n;
+        cin>>n;
+        assert(n <= 16384);
+        for (int i=1;i<=n;i++) {
+            cin>>a[i];
+            a[i]+=a[i-1];
+        }
+        cout<<solve(1,n)<<endl;
+    }
+    return 0;
+}
