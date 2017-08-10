c78022238748ff02a9da27b950d12791a242e7fa
Ganesh S Kudva <ganeshskudva@gmail.com>
2017-08-09 21:42:12 -0700

    Practice Problem
    Practice Problem

--- HackerRank/anagram.cpp
+++ HackerRank/anagram.cpp
@@ -1,63 +1,129 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <cstring>
+/*
+* AUTHOR: Ganesh S Kudva
+*/
 
-typedef unsigned long long int ull;
+#include <bits/stdc++.h>
+
+typedef long long int ull;
+
+#define MEM(a, b, c, d) memset(a, (b), c * sizeof(d))
+#define CLR(a) memset(a, 0, sizeof(a))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(X) ( (X) > 0 ? (X) : ( -(X) ) )
+#define S(X) ( (X) * (X) )
+#define SZ(V) (int )V.size()
+#define FORN(i, n) for(i = 0; i < n; i++)
+#define FORAB(i, a, b) for(i = a; i <= b; i++)
+#define ALL(V) V.begin(), V.end()
+
+#define MOD                     1000000007
+#define pb(x)                   push_back(x)
+#define mp(x,y)                 make_pair(x,y)
+
+#if 0
+typedef pair<int,int> PII;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+#endif
+
+#define IN(A, B, C) assert( B <= A && A <= C)
 
 using namespace std;
 
 #define DEBUG 0
+#define CLOCK 0
 
-ull num_chars (std::string first,
-			   std::string second)
-{
-	ull i = 0;
-	ull cnt = 0;
-	int arr_first[26];
-	int arr_second[26];
+bool isPrime(ull number){
 
-	memset(arr_first, 0, 26 * sizeof(int));
-	memset(arr_second, 0, 26 * sizeof(int));
+    if(number < 2) return false;
+    if(number == 2) return true;
+    if(number % 2 == 0) return false;
+    for(int i=3; (i*i)<=number; i+=2){
+        if(number % i == 0 ) return false;
+    }
+    return true;
 
-	for (i = 0; i < first.length(); i++)
-	{
-		arr_first[int(first[i]) - 'a']++;
-	}
-
-	for (i = 0; i < second.length(); i++)
-	{
-		arr_second[int(second[i]) - 'a']++;
-
-	}
-
-	for (i = 0; i < 26; i++)
-	{
-		cnt += abs(arr_first[i] - arr_second[i]);
-
-	}
-
-	return cnt;
 }
+
+static inline ull is_odd(ull x) { return x & 1; }
 
 int main()
 {
-	std::string first, second;
-#if 0
+#if CLOCK
+	clock_t tStart = clock();
+#endif
+	ull t = 0,i,j, n, val, cnt, size;
+	string str;
+	vector<ull> vec;
+	unordered_map<char, ull> countA, countB;
+	bool midWay, impossible;
 #if DEBUG 
 	srand((unsigned)time(0));
 	t = (rand()%10)+1; 
-	cout <<N<<endl;
+	cout <<t<<endl;
 #else 
 	cin >> t;
 #endif 
+
+	while (t--)
+	{
+		cin >> str;
+		size = str.size();
+
+		if (is_odd(size))
+		{
+			cout << "-1" << endl;
+		}
+		else
+		{
+			countA.clear();
+			countB.clear();
+			midWay = false;
+
+			FORN(i, size)
+			{
+				if (i == (size/2))
+				{
+					midWay = true;
+				}
+
+				if (midWay)
+				{
+					countB[str[i]]++;
+				}
+				else
+				{
+					countA[str[i]]++;
+				}
+
+				cnt = 0;
+				for (auto it = countB.begin(); it != countB.end(); ++it)
+				{
+					if (countA.find(it->first) == countA.end())
+					{
+						val = 0;
+					}
+					else
+					{
+						val = countA[it->first];
+					}
+
+					if (val < it->second)
+					{
+						cnt += (it->second - val);
+					}
+				}
+			}
+
+			cout << cnt << endl;
+		}
+
+	}
+
+
+#if CLOCK
+	printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 #endif
-
-	cin >> first >> second;
-
-	cout << num_chars(first, second) << endl;
-
 	return 0;
 }
