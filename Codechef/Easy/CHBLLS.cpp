69e6ebe8defe72071568ebdfb8148c8972c1dcb9
Ganesh S Kudva <ganeshskudva@gmail.com>
2017-09-06 21:36:12 -0700

    Practice Problem
    Practice Problem

---
+++ Codechef/Easy/CHBLLS.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <math.h>
+#include <assert.h>
+#include <time.h>
+#include <memory.h>
+#include <set>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <stack>
+#include <bitset>
+//#include <unordered_map>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cout << "1\n";
+    cout << "3 4 5 5\n";
+    cout << "3 1 1 2\n";
+    fflush(stdout);
+    int diff;
+    cin >> diff;
+    cout << "2\n";
+    cout << diff + 3 << "\n";
+    return 0;
+}
