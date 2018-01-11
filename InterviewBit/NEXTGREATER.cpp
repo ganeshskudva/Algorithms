56f610be9716aea04bfc65d221b22a82fdc95fb4
Ganesh S Kudva <ganeshskudva@gmail.com>
2018-01-11 12:28:36 -0800

    Checkpoint 3
    Checkpoint 3

---
+++ InterviewBit/NEXTGREATER.cpp
@@ -0,0 +1,18 @@
+/*Problem: https://www.interviewbit.com/problems/nextgreater/*/
+
+vector<int> Solution::nextGreater(vector<int> &A) {
+    stack<int>pila;
+    pila.push(0);
+    vector<int>r;
+    r.push_back(-1);
+    for(int i=1;i<A.size();i++){
+        r.push_back(-1);
+        int j=A[i];
+        while(!pila.empty()&&j>A[pila.top()]){
+            r[pila.top()]=j;
+            pila.pop();
+        }
+        pila.push(i);
+    }
+    return r;
+}
