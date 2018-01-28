846c3f695848a583a4c6e6fad2ac4d1a9bd2760f
Ganesh S Kudva <ganeshskudva@gmail.com>
2018-01-27 17:54:16 -0800

    InterviewBit Hashing
    InterviewBit Hashing

---
+++ InterviewBit/Longest_Substr_without_repeat.cpp
@@ -0,0 +1,33 @@
+/*
+ *Given a string,find the length of the longest substring without repeating characters.
+ * https://www.interviewbit.com/problems/longest-substring-without-repeat/
+ */
+int Solution::lengthOfLongestSubstring(string A) {
+    unordered_map <char,bool> mymap;
+    int i;
+    int maxLength=0;
+    int currentLength=0;
+    int p=0,q=0;
+    int n=A.length();
+    while(q<n){
+        if(mymap.find(A[q])==mymap.end()){
+            mymap[A[q]]=1;
+            q++;
+        }
+        else{
+           while(A[p]!=A[q]){
+               mymap.erase(A[p]);
+               p++;
+           }
+            if(p!=q){
+               p++;
+            }
+            mymap.erase(A[q]);
+            
+        }
+        currentLength=q-p;
+        maxLength=max(maxLength,currentLength);
+    }
+    return maxLength;
+    
+}
