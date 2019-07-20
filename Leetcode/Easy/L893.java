2a8284c253faa529c4828ec6592aac078245e9b1
Ganesh S Kudva <ganeshskudva@gmail.com>
2019-07-20 10:45:52 -0700

    LCE
    LCE

---
+++ Leetcode/Easy/L893.java
@@ -0,0 +1,13 @@
+// 893. Groups of Special-Equivalent Strings
+class Solution {
+    public int numSpecialEquivGroups(String[] A) {
+        Set<String> seen = new HashSet();
+        for (String S: A) {
+            int[] count = new int[52];
+            for (int i = 0; i < S.length(); ++i)
+                count[S.charAt(i) - 'a' + 26 * (i % 2)]++;
+            seen.add(Arrays.toString(count));
+        }
+        return seen.size();
+    }
+}
