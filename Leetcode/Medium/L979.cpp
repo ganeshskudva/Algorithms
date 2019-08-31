9d4595319270643bc48ccf34a55c240689a852ae
Ganesh S Kudva <ganeshskudva@gmail.com>
2019-08-31 12:11:03 -0700

    LCM
    LCM

---
+++ Leetcode/Medium/L979.cpp
@@ -0,0 +1,17 @@
+//979. Distribute Coins in Binary Tree
+class Solution {
+    int ans;
+    public int distributeCoins(TreeNode root) {
+        ans = 0;
+        dfs(root);
+        return ans;
+    }
+
+    public int dfs(TreeNode node) {
+        if (node == null) return 0;
+        int L = dfs(node.left);
+        int R = dfs(node.right);
+        ans += Math.abs(L) + Math.abs(R);
+        return node.val + L + R - 1;
+    }
+}
