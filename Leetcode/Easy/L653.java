e3433f437bf4958b69b49777b8313681636f75f2
Ganesh S Kudva <ganeshskudva@gmail.com>
2019-10-05 18:43:56 -0700

    LCE
    LCE

---
+++ Leetcode/Easy/L653.java
@@ -0,0 +1,34 @@
+/**
+ * 653. Two Sum IV - Input is a BST
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+public boolean findTarget(TreeNode root, int k) {
+        List < Integer > list = new ArrayList();
+        inorder(root, list);
+        int l = 0, r = list.size() - 1;
+        while (l < r) {
+            int sum = list.get(l) + list.get(r);
+            if (sum == k)
+                return true;
+            if (sum < k)
+                l++;
+            else
+                r--;
+        }
+        return false;
+    }
+    public void inorder(TreeNode root, List < Integer > list) {
+        if (root == null)
+            return;
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+}
