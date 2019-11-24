50bd1b2627b435f11fb7963a857a109e8ff422a4
Ganesh S Kudva <ganeshskudva@gmail.com>
2019-11-23 17:19:15 -0800

    LCE
    LCE

---
+++ Leetcode/Easy/L122.java
@@ -0,0 +1,11 @@
+//122. Best Time to Buy and Sell Stock II
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
+}
