ff4a34b3d19b64865398f3c8f5e5a72285306a8f
Ganesh S Kudva <ganeshskudva@gmail.com>
2019-11-28 17:58:45 -0800

    LCM
    LCM

---
+++ Leetcode/Medium/L253.java
@@ -0,0 +1,22 @@
+// 253. Meeting Rooms II
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        int[] starts = new int[intervals.length];
+        int[] ends = new int[intervals.length];
+        for(int i=0; i<intervals.length; i++) {
+            starts[i] = intervals[i][0];
+            ends[i] = intervals[i][1];
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        int rooms = 0;
+        int endsItr = 0;
+        for(int i=0; i<starts.length; i++) {
+            if(starts[i]<ends[endsItr])
+                rooms++;
+            else
+                endsItr++;
+        }
+        return rooms;
+    }
+}
