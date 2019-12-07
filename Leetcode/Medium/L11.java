63072cab8c0b28d7727ca6138cebc3012f4edc74
Ganesh S Kudva <ganeshskudva@gmail.com>
2019-12-07 11:12:40 -0800

    LCM
    LCM

---
+++ Leetcode/Medium/L11.java
@@ -0,0 +1,14 @@
+// 11. Container With Most Water
+class Solution {
+    public int maxArea(int[] height) {
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
+    }
+}
