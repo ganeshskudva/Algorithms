2ee06137e3fe485b2980a2524c070fa1ce69c4b2
Ganesh S Kudva <ganeshskudva@gmail.com>
2020-01-21 19:30:26 -0800

    LCM
    LCM

---
+++ Leetcode/Medium/L113.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList();
+        if (root == null) return res;
+        dfs(root, sum, res, new ArrayList());
+        return res;
+    }
+    private void dfs(TreeNode root, int target, List<List<Integer>> res, List<Integer> list) {
+        if (root == null) return;
+        list.add(root.val);
+        if (root.left == null && root.right == null && target == root.val)
+            res.add(new ArrayList(list));
+        dfs(root.left, target - root.val, res, list);
+        dfs(root.right, target - root.val, res, list);
+        list.remove(list.size() - 1);
+    }
+}
