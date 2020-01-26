b2d097516dd011018cdb8c0e007564d4f82723c4
Ganesh S Kudva <ganeshskudva@gmail.com>
2020-01-26 07:37:58 -0800

    LCM
    LCM

---
+++ Leetcode/Medium/L1135.java
@@ -0,0 +1,45 @@
+class Solution {
+    
+    int[] parent;
+    int n;
+    
+    private void union(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        
+        if (px != py) {
+            parent[px] = py;
+            n--;
+        }
+    }
+    
+    private int find(int x) {
+        if (parent[x] == x) {
+            return parent[x];
+        }
+        parent[x] = find(parent[x]); // path compression
+        return parent[x];
+    }
+    
+    public int minimumCost(int N, int[][] connections) {
+        parent = new int[N + 1];
+        n = N;
+        for (int i = 0; i <= N; i++) {
+            parent[i] = i;
+        }
+        
+        Arrays.sort(connections, (a, b) -> (a[2] - b[2]));
+        
+        int res = 0;
+        
+        for (int[] c : connections) {
+            int x = c[0], y = c[1];
+            if (find(x) != find(y)) {
+                res += c[2];
+                union(x, y);
+            }
+        }
+        
+        return n == 1 ? res : -1;
+    }
+}
