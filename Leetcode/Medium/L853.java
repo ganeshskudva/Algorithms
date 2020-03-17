c79b2d174071f8fb9f3fcb40c2d4de336a77619f
Ganesh S Kudva <ganeshskudva@gmail.com>
2020-03-17 13:49:32 -0700

    LCM
    LCM

---
+++ Leetcode/Medium/L853.java
@@ -0,0 +1,14 @@
+class Solution {
+  public int carFleet(int target, int[] pos, int[] speed) {
+        TreeMap<Integer, Double> m = new TreeMap<>();
+        for (int i = 0; i < pos.length; ++i) m.put(-pos[i], (double)(target - pos[i]) / speed[i]);
+        int res = 0; double cur = 0;
+        for (double time : m.values()) {
+            if (time > cur) {
+                cur = time;
+                res++;
+            }
+        }
+        return res;
+    }
+}
