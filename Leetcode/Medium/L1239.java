d1b72742ea33e2d140d73a22be300d7d5801b8f5
Ganesh S Kudva <ganeshskudva@gmail.com>
2020-03-28 11:40:22 -0700

    LCM
    LCM

---
+++ Leetcode/Medium/L1239.java
@@ -0,0 +1,41 @@
+class Solution {
+    private int result = 0;
+
+    public int maxLength(List<String> arr) {
+        if (arr == null || arr.size() == 0) {
+            return 0;
+        }
+
+        dfs(arr, "", 0);
+
+        return result;
+    }
+
+    private void dfs(List<String> arr, String path, int idx) {
+        boolean isUniqueChar = isUniqueChars(path);
+        
+        if (isUniqueChar) {
+            result = Math.max(path.length(), result);
+        }
+
+        if (idx == arr.size() || !isUniqueChar) {
+            return;
+        }
+        
+        for (int i = idx; i < arr.size(); i++) {
+            dfs(arr, path + arr.get(i), i + 1);
+        }
+    }
+
+    private boolean isUniqueChars(String s) {
+        Set<Character> set = new HashSet<>();
+
+        for (char c : s.toCharArray()) {
+            if (set.contains(c)) {
+                return false;
+            }
+            set.add(c);
+        }
+        return true;
+    }
+}
