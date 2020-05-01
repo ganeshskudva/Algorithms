af6179402ef4e4429a9702c42e15059e930585b6
Ganesh S Kudva <ganeshskudva@gmail.com>
2020-05-01 15:50:48 -0700

    LCM
    LCM

---
+++ Leetcode/Medium/L1008.java
@@ -0,0 +1,14 @@
+class Solution {
+ int i = 0;
+    public TreeNode bstFromPreorder(int[] A) {
+        return bstFromPreorder(A, Integer.MAX_VALUE);
+    }
+
+    public TreeNode bstFromPreorder(int[] A, int bound) {
+        if (i == A.length || A[i] > bound) return null;
+        TreeNode root = new TreeNode(A[i++]);
+        root.left = bstFromPreorder(A, root.val);
+        root.right = bstFromPreorder(A, bound);
+        return root;
+    }
+}
