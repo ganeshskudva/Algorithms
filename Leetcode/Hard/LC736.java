0a607ce22c5638680e361921f09dce33249bdef2
Ganesh S Kudva <ganeshskudva@gmail.com>
2020-07-22 13:29:22 -0700

    LCH
    LCH

---
+++ Leetcode/Hard/LC736.java
@@ -0,0 +1,45 @@
+class Solution {
+private int eval(String exp, Map<String, Integer> parent) {
+        if (exp.charAt(0) != '(') {
+            // just a number or a symbol
+            if (Character.isDigit(exp.charAt(0)) || exp.charAt(0) == '-')
+                return Integer.parseInt(exp);
+            return parent.get(exp);
+        }
+        // create a new scope, add add all the previous values to it
+        Map<String, Integer> map = new HashMap<>();
+        map.putAll(parent);
+        List<String> tokens = parse(exp.substring(exp.charAt(1) == 'm' ? 6 : 5, exp.length() - 1));
+        if (exp.startsWith("(a")) { // add
+            return eval(tokens.get(0), map) + eval(tokens.get(1), map);
+        } else if (exp.startsWith("(m")) { // mult
+            return eval(tokens.get(0), map) * eval(tokens.get(1), map);
+        } else { // let
+            for (int i = 0; i < tokens.size() - 2; i += 2)
+                map.put(tokens.get(i), eval(tokens.get(i + 1), map));
+            return eval(tokens.get(tokens.size() - 1), map);
+        }
+    }
+    private List<String> parse(String str) {
+        // seperate the values between two parentheses
+        List<String> res = new ArrayList<>();
+        int par = 0;
+        StringBuilder sb = new StringBuilder();
+        for (char c: str.toCharArray()) {
+            if (c == '(') par++;
+            if (c == ')') par--;
+            if (par == 0 && c == ' ') {
+                res.add(new String(sb));
+                sb = new StringBuilder();
+            } else {
+                sb.append(c);
+            }
+        }
+        if (sb.length() > 0) res.add(new String(sb));
+        return res;
+    }
+    
+        public int evaluate(String expression) {
+        return eval(expression, new HashMap<>());
+    }
+}
